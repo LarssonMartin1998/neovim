b91b452eb03edfc7c0a5883f2717523bd3104e3f
Martin Larsson <larssonmartin1998@gmail.com>
2025-01-17 13:07:36 +0000

    Add flake for building neovim from source in HM

--- flake.nix
+++ flake.nix
@@ -1,38 +1,49 @@
-{ ... } : {
-    description = "Neovim built from source at the 'latest-stable-release' tag";
+{
+  description = "Neovim built from source at the 'latest-stable-version' tag";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
 
-    outputs = { self, nixpkgs }: {
-        packages.${self.system} = let
-            pkgs = import nixpkgs { system = self.system; };
-        in
-            neovim = pkgs.stdenv.mkDerivation {
-                pname = "neovim";
-                version = "latest-stable-release";
+  outputs = { self, nixpkgs }: let
+    lib = nixpkgs.lib;
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+  in {
+    packages = lib.genAttrs supportedSystems (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in
+      pkgs.stdenv.mkDerivation {
+        pname = "neovim";
+        version = "latest-stable-version";
 
-                src = pkgs.fetchgit {
-                    url = self.url;
-                    rev = "refs/tags/latest-stable-release";
-                    sha256 = "sha256-placeholder";
-                };
+        src = builtins.fetchGit {
+          url = "https://github.com/LarssonMartin1998/neovim";
+          ref = "refs/tags/latest-stable-version";
+        };
 
-                buildInputs = with pkgs; [
-                    cmake
-                    ninja
-                    clang
-                ];
+        buildInputs = with pkgs; [
+          cmake
+          ninja
+          clang
+        ];
 
-                nativeBuildInputs = with pkgs; [ pkg-config ];
-                buildPhase = ''
-                    make CMAKE_BUILD_TYPE=Release
-                ''
+        nativeBuildInputs = with pkgs; [ pkg-config ];
 
-                installPhase = ''
-                    make install
-                ''
-            };
-    };
+        buildPhase = ''
+          make CMAKE_BUILD_TYPE=Release
+        '';
+
+        installPhase = ''
+          make install DESTDIR=$out
+        '';
+      });
+
+    # Set the default package to the `neovim` attribute for the current system
+    defaultPackage = lib.genAttrs supportedSystems (system:
+      self.packages.${system}.neovim);
+  };
 }
